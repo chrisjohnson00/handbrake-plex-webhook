47c0b4ed13a20bc1a169cb1b10078452556e36d7
Chris Johnson <csjohnson3@wsgc.com>
2020-08-02 14:00:59 -0700

    Initial app - just listening and logging requests

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # handbrake-plex-webhook
 A webhook receiver from Plex that will trigger a watched show to be downgraded in resolution
+
+## Local setup
+
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+
+## Run Locally
+
+    export FLASK_ENV=development
+    flask run
---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request
+import logging
+
+application = Flask(__name__)
+application.logger.setLevel(logging.INFO)
+required_configs = []
+
+
+@application.route('/')
+def index():
+    return 'Welcome to the Handbrake Plex Webhook Server!'
+
+
+@application.route('/webhook', methods=['GET', 'POST'])
+def web_hook():
+    application.logger.info("Web hook called")
+    application.logger.info("Web hook headers: {}".format(request.headers))
+    application.logger.info("Web hook data: {}".format(request.get_json()))
+    return 'Done'
+
+
+@application.route('/health')
+def health_check():
+    # put logic here to ensure we are happy to fulfill user requests
+    return "Success"
+
+
+@application.route('/config')
+def config():
+    application.logger.info("Rendering config page")
+    response_text = ""
+    for config in required_configs:
+        value = application.config.get(config)
+        if any(secret in config for secret in ['KEY', 'TOKEN', 'PASSWORD']):
+            response_text += "{}: [REDACTED]<br/>".format(config)
+        else:
+            response_text += "{}: {}<br/>".format(config, value)
+    return response_text
+
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=80)
